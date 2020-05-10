a0bc58f4127e286014fa7f63c5ae23f8e6bfe4bb
kanghyunji <khyunji9@naver.com>
2020-05-11 01:13:50 +0900

    checkbox : user / manager xml intent

--- app/src/main/java/com/example/subin/carepot_20200427/UserList.java
+++ app/src/main/java/com/example/subin/carepot_20200427/UserList.java
@@ -9,7 +9,5 @@ public class UserList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_list);
-
-        //test
     }
 }
