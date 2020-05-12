890d8bc7cffcd33c160bf6fa61a2e212833486c0
kanghyunji <khyunji9@naver.com>
2020-05-12 15:09:40 +0900

    (200512) checkbox : user/manager선택에 따라서  join/login DB 반영 및 화면전환

--- app/src/main/java/com/example/subin/carepot_20200427/UserList.java
+++ app/src/main/java/com/example/subin/carepot_20200427/UserList.java
@@ -1,20 +1,9 @@
 package com.example.subin.carepot_20200427;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
 public class UserList extends AppCompatActivity {
-
-    private ArrayList<MainData> arrayList;
-    private MainAdapter mainAdapter;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -22,25 +11,6 @@ public class UserList extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        recyclerView = (RecyclerView)findViewById(R.id.rv);
-        linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-
-        arrayList = new ArrayList<>();
-
-        mainAdapter = new MainAdapter(arrayList);
-        recyclerView.setAdapter(mainAdapter);
-
-        Button btn_add = (Button)findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainData mainData = new MainData(R.mipmap.ic_launcher, "이름", "주소");
-                arrayList.add(mainData);
-                mainAdapter.notifyDataSetChanged();
-            }
-        });
 
     }
 }
