79133f48a3fded4f9c54f1a33c88e77d5ef881ae
KIMSUBIN17 <tnqls24860@naver.com>
2020-06-11 14:36:35 +0900

    사용자 목록 되돌아가는 버튼 추가

--- app/src/main/java/com/example/subin/carepot_20200427/MainActivity.java
+++ app/src/main/java/com/example/subin/carepot_20200427/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.subin.carepot_20200427;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     TextView guard_phoneNum;
     TextView user_caution;
 
+    Button btn_back;
+    Button btn_clear;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,27 @@ public class MainActivity extends AppCompatActivity {
         user_address = (TextView)findViewById(R.id.text_address);
         guard_phoneNum = (TextView)findViewById(R.id.text_guard_phoneNum);
         user_caution = (TextView)findViewById(R.id.text_user_caution);
+
+        btn_back = (Button) findViewById(R.id.btn_back);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+
+
+        //사용자 목록으로 돌아가는 버튼
+        btn_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), UserList.class);
+                startActivity(intent);
+                //finish();
+
+                //Toast toast = Toast.makeText(MainActivity.this, "사용자 목록으로 이동", Toast.LENGTH_SHORT);
+                //toast.show();
+
+            }
+        });
+
+        //버튼누른횟수 데이터 초기화 버튼
 
 
         helper = new DatabaseOpenHelper(MainActivity.this, DatabaseOpenHelper.TABLE_USERS, null, version);
